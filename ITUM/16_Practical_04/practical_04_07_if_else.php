6f9fd58e401177585076dc071f1b0554b97cc25e
Madhawa Monarawila <madawammonarawila@gmail.com>
2021-05-05 21:42:14 +0530

    add practical04_07

---
+++ ITUM/16_Practical_04/practical_04_07_if_else.php
@@ -0,0 +1,21 @@
+<?php
+$d = date("D");
+if ($d == "Fri"){
+    echo "Have a nice weekend!";
+} else {
+    echo "Have a nice day!";
+}
+echo "<br>";
+// something I tested
+/* print(date("M"));
+print(date("D"));
+print(date("W"));
+print(date("Y"));
+printf(date("D"));
+ */
+/* echo "<br>";
+if ($d == "Fri") {
+    echo "Have a nice weekend!";
+} */
+
+?>
