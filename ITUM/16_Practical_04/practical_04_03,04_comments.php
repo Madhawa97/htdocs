51169ea9a3e97d579a0b1d51dc421f2cbf015a7c
Madhawa Monarawila <madawammonarawila@gmail.com>
2021-05-05 21:41:16 +0530

    add practical04_03,04

---
+++ ITUM/16_Practical_04/practical_04_03,04_comments.php
@@ -0,0 +1,11 @@
+<?php
+    # This is comment 01
+    //This is comment 02
+    echo "single comment";
+
+    /* This is a 
+    multi 
+    line 
+    comment */
+    echo "multi-line comment";
+?>
