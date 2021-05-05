b0a7960bf4795b5c2b0d56188850e67572e67065
Madhawa Monarawila <madawammonarawila@gmail.com>
2021-05-05 21:42:03 +0530

    add practical04_06

---
+++ ITUM/16_Practical_04/practical_04_06_casesensitivity copy.php
@@ -0,0 +1,10 @@
+<?php
+$color = "Blue";
+$Color = "Red";
+$COLOR = "Green";
+
+// display var value
+echo "The color of the sky is " . $color . "<br>";
+echo "The color of the sun is " . $Color . "<br>";
+echo "The color of the tree is " . $COLOR . "<br>";
+?>
